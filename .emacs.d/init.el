996eac69a53496df3afc0b78d12084092aa48551
kumpeishiraishi <kumpeishiraishi@gmail.com>
2019-04-01 17:13:00 +0900

    init.org directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,3 @@
 (require 'org-install)
-(defvar my-config-dir (concat user-emacs-directory "./../dotfiles/.emacs.d/init_org/"))
+(defvar my-config-dir (concat (substitute-in-file-name "$HOME") "/dotfiles/.emacs.d/init_org/"))
 (org-babel-load-file (expand-file-name "init.org" my-config-dir))
