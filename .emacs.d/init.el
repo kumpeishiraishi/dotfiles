a64406ee7b49b781ee6b7c072742e126b2547906
kumpeishiraishi <kumpeishiraishi@gmail.com>
2019-04-02 10:47:30 +0900

    Quit init.org-directory variable when loading

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,2 @@
 (require 'org-install)
-(defvar my-config-dir (concat (substitute-in-file-name "$HOME") "/dotfiles/.emacs.d/init_org/"))
-(org-babel-load-file (expand-file-name "init.org" my-config-dir))
+(org-babel-load-file (concat (substitute-in-file-name "$HOME") "/dotfiles/.emacs.d/init_org/init.org"))
