fae1c3465fe76956b54bdb32dab9ff653535a9e7
kumpeishiraishi <kumpeishiraishi@gmail.com>
2019-04-13 00:09:06 +0900

    Add Homebrew installation line

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
 ln -s ~/dotfiles/.aspell.en.pws ~/.aspell.en.pws
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
-ln -s ~/.w3m/config ~/.w3m/config
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+#if [ $(uname)=='Darwin' ]
+#then
+#    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#fi
