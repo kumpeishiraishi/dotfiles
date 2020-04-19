376ba1cdbcb9bcfad3b038be715a34d57b614207
kumpeishiraishi <kumpeishiraishi@gmail.com>
2020-04-19 20:45:35 +0900

    Add words to aspell. Modify C++ settings in Emacs. Set PATH etc.

--- install.sh
+++ install.sh
@@ -6,7 +6,13 @@ ln -s ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
-#if [ $(uname)=='Darwin' ]
-#then
-#    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#fi
+if [ $(uname)=='Darwin' ]
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew install aspell cmigemo emacs gcc git gnuplot imagemagick nkf pandoc python3 screen stunnel ripgrep zsh zsh-completions cmake llvm hugo
+    brew cask install bibdesk dropbox emacs mactex qlmarkdown selfcontrol qlstephen
+fi
+
+pip3 install numpy scipy matplotlib pandas jupyterlab Cython pip ipython
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
